7eb40179dc65c277ae1d00b9330dc05d5f71ad3f
allanlimo02 <allanlimo02@gmail.com>
2023-02-02 10:09:11 +0300

    updated stubs maps

---
+++ src/main/java/com/mekakisafaris/bookings/service/SubjectServices.java
@@ -0,0 +1,18 @@
+package com.mekakisafaris.bookings.service;
+
+import com.mekakisafaris.bookings.entity.Subjects;
+import com.mekakisafaris.bookings.repositories.SubjectRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.security.auth.Subject;
+
+@Service
+public class SubjectServices {
+//    @Autowired
+//    SubjectRepo subjectRepo;
+//    // find by Id
+//    public Subject findSubject (Subjects subjects){
+//        return subjectRepo.findById(subjects.)
+//    }
+}
